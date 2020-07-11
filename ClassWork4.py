2f21d762255a59f71b928fdb95b82f73f453706e
Halyna Romanyshyn <galyna19@gmail.com>
2020-07-12 00:36:30 +0300

    add item 4 5 ClassWork4 and add ClasWork5

--- ClassWork4.py
+++ ClassWork4.py
@@ -1,27 +1,49 @@
-print ('====ClassWork #1====')
-num1 = int(input('input number #1: '))
-num2 = int(input('input number #2: '))
-if num1 > num2:
-    print (num1,'more than', num2)
-elif num1 == num2:
-    print (num1,'is equal', num2)
-else:
-    num1 < num2
-    print (num2,'more than', num1)
+# print ('====ClassWork #1====')
+# num1 = int(input('input number #1: '))
+# num2 = int(input('input number #2: '))
+# if num1 > num2:
+#     print (num1,'more than', num2)
+# elif num1 == num2:
+#     print (num1,'is equal', num2)
+# else:
+#     num1 < num2
+#     print (num2,'more than', num1)
 
-print ('====ClassWork #2====')
-num = int(input('input number: '))
-if num%2 == 0:
-    print (num,'is even number')
-else:
-    print (num,'is odd number')
+# print ('====ClassWork #2====')
+# num = int(input('input number: '))
+# if num%2 == 0:
+#     print (num,'is even number')
+# else:
+#     print (num,'is odd number')
 
 
-print ('====ClassWork #3====')
-num = int(input('input number: '))
-a = 0
-b = 1
-for count in range(num):
-    a += 1
-    b *= a
-print (num, '! -', b,)
+# print ('====ClassWork #3====')
+# num = int(input('input number: '))
+# a = 0
+# b = 1
+# for count in range(num):
+#     a += 1
+#     b *= a
+# print (num, '! -', b,)
+
+# print ('====ClassWork #4 (while)====')
+
+# i=2 
+# while i < 100:
+#     print(i)
+#     i = i + 2 
+
+# print ('====ClassWork #4 (for)====')
+
+# for i in range(2,100,2):
+#     print (i)
+
+# print ('====ClassWork #5 ====')
+# for i in range(1,100,2):
+#     print (i)
+
+# print ('====ClassWork #4 (continue)====')
+# for i in range(1,100):
+#     if i%2 == 0:
+#         continue
+#     print (i)
