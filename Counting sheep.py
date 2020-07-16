4c99c9eb0bca4ad51d6166c5d707079680e53327
Halyna Romanyshyn <galyna19@gmail.com>
2020-07-16 20:32:53 +0300

    add task 5 HW 4

--- Counting sheep.py
+++ Counting sheep.py
@@ -1,17 +1,2 @@
-array1 = [True,  True,  True,  False,
-          True,  True,  True,  True ,
-          True,  False, True,  False,
-          True,  False, False, True ,
-          True,  True,  True,  True ,
-          False, False, True,  True ]
-
 def count_sheeps(array1): 
     return array1.count (True)
-
-print (count_sheeps(array1))
-
-
-
-
-
-# test.assert_equals(count_sheeps(array1), 17, "There are 17 sheeps in total, not %s" % count_sheeps(array1))
